b6358896253f7e24cc6ea29eb78c4068872c6c13
Bolean <wodeln@163.com>
2018-05-10 11:30:20 +0800

    事件添加考生ID

--- cprsim-model/src/main/java/com/bolean/entity/Event.java
+++ cprsim-model/src/main/java/com/bolean/entity/Event.java
@@ -23,6 +23,16 @@ public class Event implements Serializable {
 
     private String eventName;
 
+    private Long userId;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
     public Long getLogId() {
         return logId;
     }
